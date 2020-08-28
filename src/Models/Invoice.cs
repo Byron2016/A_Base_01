5b2628eb8768a1aa540e8bf929e4aa31947eb83b
Byron2016 <bgva2005@yahoo.com>
2020-08-28 02:19:11 -0500

    03 Creación Clases del Modelo en Bibliteca Models

---
+++ src/Models/Invoice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    class Invoice
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public Client Client { get; set; }
+        public decimal Iva { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal Total { get; set; }
+    }
+}
